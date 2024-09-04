e81c4c3cd9906b1513f75afcfea3d10bb060e138
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-09-05 00:11:25 +0200

    Fix error

--- src/classic/bind_old_products.template.ts
+++ src/classic/bind_old_products.template.ts
@@ -1,16 +1,16 @@
-import { Address, log } from "@graphprotocol/graph-ts"
+import { Address, log, ethereum } from "@graphprotocol/graph-ts"
 import { addIgnoredContract } from "../common/entity/ignored"
 import { BeefyERC20Product as BeefyERC20ProductTemplate } from "../../generated/templates"
-import { ADDRESS_ZERO } from "../common/utils/address";
 
 
-export function bindOldProducts(): void {
-    log.warning("Binding old boosts", [])
+
+export function bindOldProducts(block: ethereum.Block): void {
+    log.warning("Binding old boosts at block {}", [block.number.toString()])
 {{#old_boosts}}
     addIgnoredContract(Address.fromString("{{.}}"));
 {{/old_boosts}}
     
-    log.warning("Binding old vaults", [])
+    log.warning("Binding old vaults at block {}", [block.number.toString()])
 {{#old_vaults}}
     BeefyERC20ProductTemplate.create(Address.fromString("{{.}}"))
 {{/old_vaults}}
