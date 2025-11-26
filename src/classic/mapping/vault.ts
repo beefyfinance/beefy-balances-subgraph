6e23e52619e6d132e084f3871e7fd9a101315983
prevostc <998369+prevostc@users.noreply.github.com>
2025-11-26 15:06:57 +0100

    Remove the need for a classic vault init config

--- src/classic/mapping/vault.ts
+++ src/classic/mapping/vault.ts
@@ -1,7 +1,13 @@
-import { Initialized as VaultInitialized } from "../../../generated/ClassicVaultFactory/ClassicVault"
+import { Initialized as VaultInitializedWithInitialized } from "../../../generated/ClassicVaultFactory/ClassicVault"
+import { OwnershipTransferred as VaultInitializedWithOwnershipTransferred } from "../../../generated/ClassicVaultFactory/ClassicVault"
 import { BeefyERC20Product as BeefyERC20ProductTemplate } from "../../../generated/templates"
 
-export function handleClassicVaultInitialized(event: VaultInitialized): void {
+export function handleClassicVaultInitializedWithInitialized(event: VaultInitializedWithInitialized): void {
+  const address = event.address
+  BeefyERC20ProductTemplate.create(address)
+}
+
+export function handleClassicVaultInitializedWithOwnershipTransferred(event: VaultInitializedWithOwnershipTransferred): void {
   const address = event.address
   BeefyERC20ProductTemplate.create(address)
 }
